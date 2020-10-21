e6b48eec7a4b9226f64dd046c52587c48b4fc22d
iceman951 <47938031+iceman951@users.noreply.github.com>
2020-10-21 14:45:06 +0700

    first commit
    esp8266 + dust sensor
    Line notify dust density

---
+++ esp8266.ino
@@ -0,0 +1,133 @@
+void Line_Notify(String message) ;
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecureAxTLS.h> // กรณีขึ้น Error ให้เอาบรรทัดนี้ออก
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);   //Module IIC/I2C Interface บางรุ่นอาจจะใช้ 0x3f
+
+int measurePin = A0;
+int ledPower = D5;    //Pin LED
+
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+
+// Config connect WiFi
+#define WIFI_SSID "YOUR WIFINAME"
+#define WIFI_PASSWORD "YOUR WIFIPASSWORD"
+
+// Line config
+#define LINE_TOKEN "LINE ACCESS TOKEN"
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(ledPower, OUTPUT);
+  lcd.begin();
+  lcd.backlight();       // เปิด backlight
+
+  WiFi.mode(WIFI_STA);
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  digitalWrite(ledPower, LOW); // power on the LED
+  delayMicroseconds(samplingTime);
+
+  voMeasured = analogRead(measurePin); // read the dust value
+
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower, HIGH); // turn the LED off
+  delayMicroseconds(sleepTime);
+
+  // 0 - 3.3V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (3.3 / 1024);
+
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = 0.17 * calcVoltage - 0.1;
+
+  Serial.print("Raw Signal Value (0-1023): ");
+  Serial.print(voMeasured);
+
+  Serial.print(" - Voltage: ");
+  Serial.print(calcVoltage);
+
+  if (dustDensity <= 0.00) {
+    dustDensity = 0.00;
+  }
+
+  dustDensity = dustDensity * 1000;
+
+  if (dustDensity > 1000) {
+    while(dustDensity > 1000) delay(10);
+  
+    Serial.println("Enter !");
+    Line_Notify(String(dustDensity));
+    // Serial.println();
+  }
+  delay(10);
+
+  Serial.print(" - Dust Density: ");
+  Serial.print(dustDensity);
+  Serial.println(" µg/m³");
+  lcd.home();
+  lcd.setCursor(1, 0);
+  lcd.print("Dust Density ");
+  lcd.setCursor(2, 1);
+  lcd.print(dustDensity);
+  lcd.print(" ug/m3  ");
+  delay(1000);
+}
+
+void Line_Notify(String message) {
+  axTLS::WiFiClientSecure client; // กรณีขึ้น Error ให้ลบ axTLS:: ข้างหน้าทิ้ง
+
+  if (!client.connect("notify-api.line.me", 443)) {
+    Serial.println("connection failed");
+    return;   
+  }
+
+  String req = "";
+  req += "POST /api/notify HTTP/1.1\r\n";
+  req += "Host: notify-api.line.me\r\n";
+  req += "Authorization: Bearer " + String(LINE_TOKEN) + "\r\n";
+  req += "Cache-Control: no-cache\r\n";
+  req += "User-Agent: ESP8266\r\n";
+  req += "Connection: close\r\n";
+  req += "Content-Type: application/x-www-form-urlencoded\r\n";
+  req += "Content-Length: " + String(String("message=" + message).length()) + "\r\n";
+  req += "\r\n";
+  req += "message=" + message;
+  // Serial.println(req);
+  client.print(req);
+    
+  delay(20);
+
+  // Serial.println("-------------");
+  while(client.connected()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      break;
+    }
+    //Serial.println(line);
+  }
+  // Serial.println("-------------");
+}
